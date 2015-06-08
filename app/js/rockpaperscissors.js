b8d182e5b09018fddf905e3812869571e6caa2e5
Maksim Hranin <maksimhn@users.noreply.github.com>
2015-06-07 21:34:27 -0400

    Update rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,35 +21,51 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+var playerMove, computerMove;
+// These variables weren't declared in the original template
+// so repl.it spits out a mistake without me declaring them. Not sure
+// if choosing global scope is the only way to go here, but it just
+// didn't work otherwise, and I don't understand scope properly to use it right.
+
+function getPlayerMove(move) {  // Getting an input from a player.
+    return playerMove = move || getInput();
 }
 
-function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+function getComputerMove(move) {  // Getting an input from a computer.
+    return computerMove = move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
+function getWinner(playerMove,computerMove) { // Determining a winner/tie.
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove === computerMove) {        // Checking if it's a tie first looked more efficient
+      winner = "tie";                         // because we cut out a branch of multiple comparisons.
+    } else if (
+    (playerMove === "rock" && computerMove === "scissors") ||
+    (playerMove === "scissors" && computerMove === "paper") ||
+    (playerMove === "paper" && computerMove === "rock")) {
+      winner = "player";
+    } else {
+      winner = "computer";
+    }
     return winner;
 }
 
-function playToFive() {
+function playToFive() { // Playing until either side wins 5 times.
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5) {
+      getPlayerMove(getInput());
+      getComputerMove(randomPlay());
+      if (getWinner(playerMove, computerMove) === "player") {
+        playerWins++;
+      } else if (getWinner(playerMove, computerMove) === "computer") {
+        computerWins++;
+      }
+      console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+      console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
 
+// playToFive();
